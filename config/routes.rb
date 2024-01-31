60c4124b007ab9139e854574a85d157f87c5d3a6
mbialowas10 <mbialowas@rrc.ca>
2024-01-31 09:33:36 -0600

    s_251246_Jan_31_Search implemented

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,12 @@ Rails.application.routes.draw do
   # get 'production_companies/show'
   # get 'movies/index'
   # get 'movies/show'
-  resources :movies, only: [:index, :show]
+  resources :movies, only: [:index, :show] do
+    collection do
+      get "search" # movies/search/:format
+    end
+  end
+
   resources :production_companies, only: [:index, :show]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
